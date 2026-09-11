e71506cafde326fd71f70546425a5b5999d9c494
Rosivan <rosivanac@hotmail.com>
2026-09-11 18:58:51 -0300

    Criado a Subtração

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,13 @@
-print("### Calculadora de Média ###")
+print("### Calculadora PYTHON ###")
 
-def soma(num1, num2):
-    soma = num1 + num2
-    return soma
+# def soma(num1, num2):
+#     soma = num1 + num2
+#     return soma
 
-print("Resultado da soma: ", soma(10, 5))
+# print("Resultado da soma: ", soma(10, 5))
+
+def subtracao(num1, num2):
+    subtracao = num1 - num2
+    return subtracao
+
+print("Resultado da subtração: ", subtracao(10, 5))
