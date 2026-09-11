ddcafb2a6f2b5aadf24a92bc1a72d3815c339420
Rosivan <rosivanac@hotmail.com>
2026-09-11 19:15:29 -0300

    Criado a funcao de Multiplicação

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,6 @@
 print("### Calculadora PYTHON ###")
 
-# def soma(num1, num2):
-#     soma = num1 + num2
-#     return soma
-
-# print("Resultado da soma: ", soma(10, 5))
-
-def subtracao(num1, num2):
-    subtracao = num1 - num2
-    return subtracao
-
-print("Resultado da subtração: ", subtracao(10, 5))
+def multiplicacao(num1, num2):
+    multiplicacao = num1 * num2
+    return multiplicacao
+print("A multiplicação é: ", multiplicacao(10, 5))
